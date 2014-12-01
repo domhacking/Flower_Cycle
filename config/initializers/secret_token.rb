c4f4bc2ea5341e27f8a95cc6e856e4dbe967fe15
domhacking <dominichacking@gmail.com>
2014-12-01 15:38:54 +0000

    first commit, devise installed, models to scaffolded next

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlowercycleApp::Application.config.secret_token = '378ebebefd925c84efe9d5124a68a7d14e3d833c284d768537bdb3f5b217d9a3fd4909271c6115fd06d17f5879930a119a1b4da826b608aa2a818e7235d8025d'
