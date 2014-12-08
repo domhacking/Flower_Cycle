2994a3c3f2ccecf480be82271714d3303d38147c
domhacking <dominichacking@gmail.com>
2014-12-08 16:20:31 +0000

    Added Ajax to Bouquets. You can add edit, delete as an admin

--- app/assets/javascripts/bouquets.js
+++ app/assets/javascripts/bouquets.js
@@ -22,46 +22,78 @@ railsBouquet.bouquetForm = function(){
     dataType: "json"
   }).success(function(data){
     console.log(data)
-      bouquets.getPosts();
+    railsBouquet.getBouquets();
       // bouquets.togglePages('#all_bouquets');
-
-  });
+    });
 }
 
 railsBouquet.getBouquets = function(){
   //   //Clear the posts table to avoid  duplicates posts;
-  //   $('#all_bouquets table tbody').empty();
+  $('#listbouquets').empty();
   $.ajax({
     url: '/bouquets',
     type: "GET",
     dataType:"json"
-    }).success(function(data){
-      console.log('hello')
-      $.each(data, function(index, item){
-        var listBouquets = $("<ul>"+
-            "<li>" + item.bouquet_name+"</li>"+
-            "<li>" + item.price+"</li>"+
-            "<li>" + item.bouquet_description +"</li>"+
-            "<li><a href=''>" + item.website +"</a></li>"+
-            "<td><button data-id='" + item.id+"' class='btn edit_post'>Edit</button> <button data-id='" + item.id+"' class='btn btn-danger delete_post'>Delete</button></td>"+
-            "</tr>");
+  }).success(function(data){
+    
+    $.each(data, function(index, item){
+      var listBouquets = $("<ul>"+
+        "<li>" + item.bouquet_name+"</li>"+
+        "<li>" + item.price+"</li>"+
+        "<li>" + item.bouquet_description +"</li>"+
+        "<li><a href=''>" + item.website +"</a></li>"+
+        "<td><button data-id='" + item.id+"' class='btn edit_post'>Edit</button> <button data-id='" + item.id+"' class='btn btn-danger delete_post'>Delete</button></td>"+
+        "</tr>");
 
-          listBouquets.appendTo("#listbouquets")
-      });
-      console.log(data);
-    })
-  }
+      listBouquets.appendTo("#listbouquets")
+    });
+  });
+}    
 
+railsBouquet.deleteBouquet = function(){
 
+  $this = $(this);
+  var bouquetId = $(this).data('id');
+
+  $.ajax({
+    url: '/bouquets/' + bouquetId,
+    type: 'DELETE',
+    dataType: 'json',
+  }).success(function(data){
+    $this.closest('ul').remove();
+  });
+}
+
+railsBouquet.editBouquet = function() {
+  // $this = $(this);
+
+  var bouquetId = $(this).data('id');
   
+  $.ajax({
+    url: '/bouquets/' + bouquetId,
+    type: 'GET',
+    dataType: 'json'
+  })
+  .success(function(data) {
+    console.log(data);
+    $('#bouquet_name').val(data.bouquet_name);
+    $('#bouquet_description').val(data.bouquet_description);
+    $('#price').val(data.bouquet_price);
+    $('#number_bouquets').val(data.number_bouquets);
+  })
+}
+
 
 $(function(){
- $('.bouquet_submit').click(function(event){
+  $('.bouquet_submit').click(function(event){
+    event.preventDefault()
+    railsBouquet.bouquetForm()
+    console.log('hello')
+  });
   event.preventDefault()
-  railsBouquet.bouquetForm()
- });
-  event.preventDefault()
-  railsBouquet.getBouquets(); 
+  railsBouquet.getBouquets();
+  $('#listbouquets').on('click', '.delete_post', railsBouquet.deleteBouquet);
+  $('#listbouquets').on('click', '.edit_post', railsBouquet.editBouquet); 
 })
 
 
