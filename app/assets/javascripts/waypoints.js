eb4e0f048286b17ec50e23b46ec830d5f27488f5
domhacking <dominichacking@gmail.com>
2014-12-12 09:39:43 +0000

    fixing precompile error please? (jeremy) BOOM!

--- app/assets/javascripts/waypoints.js
+++ app/assets/javascripts/waypoints.js
@@ -1,27 +1,26 @@
 $(function() { 
   
   var $nav = $("nav");
-  var $nav_links = $("nav a")
+  var $nav_links = $("nav a");
 
-  var $nav_one = $("#nav_one")
+  var $nav_one = $("#nav_one");
   var $nav_two = $("#nav_two")
-  var $nav_three = $("#nav_three")
+  var $nav_three = $("#nav_three");
 
 
-  var $section_one = $("#one")
-  var $section_two = $("#two")
-  var $section_three = $("#three")
-  var $section_three = $("#four")
+  var $section_one = $("#one");
+  var $section_two = $("#two");
+  var $section_three = $("#three");
+  var $section_three = $("#four");
 
   $nav_links.click( function(event) {
-    console.log('click')
-    event.preventDefault()
+    console.log('click');
+    event.preventDefault();
     $.scrollTo(
       $(this).attr("href"),
       {
         duration: 1000,
         offset: { 'left':0, 'top':0 }
-      }
-    )
-  })
-}
+      });
+  });
+});
